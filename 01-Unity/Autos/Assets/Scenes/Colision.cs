ab56003983c675cd78adecccf4272749a0f7c796
steevenpanchi13 <steven.panchi13@gmail.com>
2024-12-07 21:10:07 -0500

    Last Changes

--- 01-Unity/Autos/Assets/Scenes/Colision.cs
+++ 01-Unity/Autos/Assets/Scenes/Colision.cs
@@ -5,6 +5,14 @@ using System.Collections.Generic;
 
 public class Colision: MonoBehaviour
 {
+    [SerializeField] private float destroyDelay = 0.5f;
+    private bool hasPackage;
+    private SpriteRenderer _spriteRenderer;
+    
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
             Debug.Log("GOLPEEEE");
@@ -12,13 +20,18 @@ public class Colision: MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Entrando a trigger");
-        if(other.tag == "Paquete")
+        if(other.tag == "Paquete" && hasPackage != true)
         {
             Debug.Log("Paquete Recogido");
+            hasPackage = true;
+            Destroy(other.gameObject,destroyDelay);
         }
-        if(other.tag == "Cliente")
+        if(other.tag == "Cliente" && hasPackage == true)
         {
             Debug.Log("Paquete Entregado");
+            hasPackage = false;
+
+            _spriteRenderer.color = Color.green;
         }
        
     }
